d3f53fefcd8a065822c3cc4f975f2dba0a851539
Jayaram Kancherla <jayaram.kancherla@gmail.com>
2023-07-28 08:40:32 -0700

    lint, docstrings and dead code

--- src/scranpy/__init__.py
+++ src/scranpy/__init__.py
@@ -14,5 +14,3 @@ except PackageNotFoundError:  # pragma: no cover
     __version__ = "unknown"
 finally:
     del version, PackageNotFoundError
-
-__all__ = ["quality_control"]
--- tests/test_quality_control_steps.py
+++ tests/test_quality_control_steps.py
@@ -1,14 +1,14 @@
 import numpy as np
-import scranpy as scr
 from scranpy.quality_control import per_cell_rna_qc_metrics
 
 __author__ = "ltla, jkanche"
 __copyright__ = "ltla, jkanche"
 __license__ = "MIT"
 
+
 def test_quality_control_numpy():
     x = np.random.rand(1000, 100)
-    result = per_cell_rna_qc_metrics(x, subsets={ "foo": [1, 10, 100] })
+    result = per_cell_rna_qc_metrics(x, subsets={"foo": [1, 10, 100]})
 
     assert result is not None
     assert result.dims[0] == 100
@@ -24,8 +24,10 @@ def test_quality_control_numpy():
     assert result0.column("subset_proportions").shape[1] == 0
 
     # Same results when running in parallel.
-    resultp = per_cell_rna_qc_metrics(x, subsets={ "BAR": [1, 10, 100] }, num_threads = 3)
+    resultp = per_cell_rna_qc_metrics(x, subsets={"BAR": [1, 10, 100]}, num_threads=3)
     assert np.array_equal(result.column("sums"), resultp.column("sums"))
     assert np.array_equal(result.column("detected"), resultp.column("detected"))
-    assert np.array_equal(result.column("subset_proportions").column(0), resultp.column("subset_proportions").column(0))
-
+    assert np.array_equal(
+        result.column("subset_proportions").column(0),
+        resultp.column("subset_proportions").column(0),
+    )
