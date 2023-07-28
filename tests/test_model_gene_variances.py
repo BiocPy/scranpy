478813ab3d68873062259a52733c59a153955e8b
Jayaram Kancherla <jayaram.kancherla@gmail.com>
2023-07-28 15:52:34 -0700

    update tests

--- tests/test_model_gene_variances.py
+++ tests/test_model_gene_variances.py
@@ -12,6 +12,10 @@ def test_numpy_no_blocks():
     res = model_gene_variances(x)
 
     assert res is not None
+    assert res.column("means") is not None
+    assert res.column("variances") is not None
+    assert res.column("fitted") is not None
+    assert res.column("residuals") is not None
 
 
 def test_numpy_blocks():
@@ -25,3 +29,10 @@ def test_numpy_blocks():
     resblock = model_gene_variances(x, block=block)
 
     assert resblock is not None
+    assert resblock.column("means") is not None
+    assert resblock.column("variances") is not None
+    assert resblock.column("fitted") is not None
+    assert resblock.column("residuals") is not None
+    assert resblock.column("per_block") is not None
+
+    assert resblock.column("per_block").shape[1] == len(block_levels)
