0bce17134c3137e844e050687e075456a0483f20
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-08-01 13:41:29 -0700

    Added proper error handling to all the parallelization overrides.
    Removed old emscripten defines from copy-paste.

--- src/scranpy/lib/parallel.h
+++ src/scranpy/lib/parallel.h
@@ -1,33 +1,11 @@
 #ifndef PARALLEL_H
 #define PARALLEL_H
 
-#ifdef __EMSCRIPTEN_PTHREADS__
+#include "tatami/tatami.hpp"
 #include <thread>
 #include <cmath>
 #include <vector>
-
-template<class Function_, typename Index_>
-void run_parallel_new(Function_ fun, Index_ njobs, int nthreads) {
-    if (nthreads == 1) {
-        fun(0, 0, njobs);
-        return;
-    }
-
-    Index_ jobs_per_worker = njobs/nthreads + (njobs % nthreads > 0);
-    std::vector<std::thread> workers;
-    workers.reserve(nthreads);
-    Index_ first = 0;
-
-    for (int w = 0; w < nthreads && first < njobs; ++w) {
-        int len = std::min(jobs_per_worker, njobs - first);
-        workers.emplace_back(fun, w, first, len);
-        first += jobs_per_worker;
-    }
-
-    for (auto& wrk : workers) {
-        wrk.join();
-    }
-}
+#include <stdexcept>
 
 template<typename Index_, class Function_>
 void run_parallel_old(Index_ njobs, Function_ fun, size_t nthreads) {
@@ -39,16 +17,31 @@ void run_parallel_old(Index_ njobs, Function_ fun, size_t nthreads) {
     Index_ jobs_per_worker = njobs/nthreads + (njobs % nthreads > 0);
     std::vector<std::thread> workers;
     workers.reserve(nthreads);
-    int first = 0;
+    std::vector<std::string> errors(nthreads);
 
+    int first = 0;
     for (int w = 0; w < nthreads && first < njobs; ++w) {
         int last = first + std::min(jobs_per_worker, njobs - first);
-        workers.emplace_back(fun, first, last);
+        workers.emplace_back([&](int w, Index_ first, Index_ len) -> void {
+            try {
+                fun(first, last);
+            } catch (std::exception& e) {
+                errors[w] = e.what();
+            } catch (...) {
+                errors[w] = "unknown C++ error";
+            }
+        }, w, first, last);
         first += jobs_per_worker;
     }
 
     for (auto& wrk : workers) {
         wrk.join();
+    }
+
+    for (const auto& e : errors) {
+        if (!e.empty()) {
+            throw std::runtime_error(e);
+        }
     }
 }
 
@@ -61,13 +54,28 @@ void run_parallel_simple(int nthreads, Function_ fun) {
 
     std::vector<std::thread> workers;
     workers.reserve(nthreads);
+    std::vector<std::string> errors(nthreads);
 
     for (int w = 0; w < nthreads; ++w) {
-        workers.emplace_back(fun, w);
+        workers.emplace_back([&](int w) -> void {
+            try {
+                fun(w);
+            } catch (std::exception& e) {
+                errors[w] = e.what();
+            } catch (...) {
+                errors[w] = "unknown C++ error";
+            }
+        }, w);
     }
 
     for (auto& wrk : workers) {
         wrk.join();
+    }
+
+    for (const auto& e : errors) {
+        if (!e.empty()) {
+            throw std::runtime_error(e);
+        }
     }
 }
 
@@ -85,13 +93,11 @@ void run_parallel_simple(int nthreads, Function_ fun) {
  * some form of *_CUSTOM_PARALLEL macro.
  */
 
-#define TATAMI_CUSTOM_PARALLEL run_parallel_new
 #define IRLBA_CUSTOM_PARALLEL run_parallel_simple
 #define KMEANS_CUSTOM_PARALLEL run_parallel_old
-#define SCRAN_CUSTOM_PARALLEL run_parallel_new
+#define SCRAN_CUSTOM_PARALLEL tatami::parallelize
 #define MNNCORRECT_CUSTOM_PARALLEL run_parallel_old
 #define QDTSNE_CUSTOM_PARALLEL run_parallel_old
 #define UMAPPP_CUSTOM_PARALLEL run_parallel_old
 
-#endif
 #endif
