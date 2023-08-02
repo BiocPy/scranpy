cf676a3529715773fdb31f743552af1cfb5f8a89
Jayaram Kancherla <jayaram.kancherla@gmail.com>
2023-08-02 13:10:14 -0700

    minor edits

--- src/scranpy/feature_selection/choose_hvgs.py
+++ src/scranpy/feature_selection/choose_hvgs.py
@@ -1,5 +1,7 @@
 import numpy as np
+
 from .. import cpphelpers as lib
+
 
 def choose_hvgs(stat: np.ndarray, number: int = 2500) -> np.ndarray:
     """Choose highly variable genes.
@@ -9,21 +11,18 @@ def choose_hvgs(stat: np.ndarray, number: int = 2500) -> np.ndarray:
             where larger values correspond to higher variability.
             This usually contains the residuals of the fitted
             mean-variance trend.
-        number (int): Number of HVGs to pick.
+        number (int): Number of HVGs to pick. Defaults to 2500.
 
     Return:
-        Array of booleans of length equal to `stat`, specifying whether a given
-        gene is considered as highly variable.
+        np.ndarray: Array of booleans of length equal to `stat`,
+        specifying whether a given gene is considered as highly variable.
     """
 
     output = np.zeros(len(stat), dtype=np.uint8)
     stat_internal = stat.astype(np.float64, copy=False)
 
     lib.choose_hvgs(
-        len(stat_internal), 
-        stat_internal.ctypes.data,
-        number,
-        output.ctypes.data
+        len(stat_internal), stat_internal.ctypes.data, number, output.ctypes.data
     )
 
     return output.astype(np.bool_, copy=False)
