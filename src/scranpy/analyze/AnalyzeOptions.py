a71aa2f900975623dcab62a5f2556dd1e6253466
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-08-18 15:25:32 -0700

    Cleaned up the options.

--- src/scranpy/analyze/AnalyzeOptions.py
+++ src/scranpy/analyze/AnalyzeOptions.py
@@ -21,26 +21,70 @@ class MiscellaneousOptions:
             Prefix for mitochondrial genes, under the assumption that the feature names are gene symbols.
             If True, the default prefix in :py:meth:`~scranpy.quality_control.rna.guess_mito_from_symbols` is used.
             If False, no attempt is made to guess the identities of mitochondrial genes.
+
+        block (Sequence, optional): 
+            Block assignment for each cell.
+            This should have length equal to the total number of cells in the dataset, before any quality control is applied.
     """
 
     snn_graph_multilevel_resolution: int = 1
     mito_prefix: Union[bool, str] = "mt-"
+    block: Optional[Sequence] = None 
 
 @dataclass
 class AnalyzeOptions:
     """Optional parameters for all :py:meth:`~scranpy.analyze.analyze.analyze` steps.
 
     Optional parameters for each function are named after the function with the ``_options`` suffix.
-    In most cases, these can be modified directly to refine the behavior of the
-    :py:meth:`~scranpy.analyze.analyze.analyze` function.
+    In most cases, these can be modified directly to refine the behavior of the :py:meth:`~scranpy.analyze.analyze.analyze` function.
+    However, for a few options, it usually makes more sense to set them across multiple parameter objects simultaneously;
+    check out the setter methods of this class for more details.
 
-    However, for a few options, it makes more sense to set them across multiple parameter objects simultaneously.
-    We provide the following methods in such cases:
-    
-    - :py:meth:`~scranpy._analyze.AnalyzeOptions.set_seed`, to set the random seed. 
-    - :py:meth:`~scranpy._analyze.AnalyzeOptions.set_block`, to set the block assignments.
-    - :py:meth:`~scranpy._analyze.AnalyzeOptions.set_threads`, to set the number of threads.
-    - :py:meth:`~scranpy._analyze.AnalyzeOptions.set_verbose`, to set the verbosity level.
+    Attributes:
+        per_cell_rna_qc_metrics_options (PerCellRnaQcMetricsOptions):
+            Options to pass to :py:meth:`~scranpy.quality_control.rna.per_cell_rna_qc_metrics`.
+
+        suggest_rna_qc_filters_options (SuggestRnaQcFiltersOptions):
+            Options to pass to :py:meth:`~scranpy.quality_control.rna.suggest_rna_qc_filters`.
+
+        create_rna_qc_filter_options (CreateRnaQcFilterOptions):
+            Options to pass to :py:meth:`~scranpy.quality_control.rna.create_rna_qc_filter`.
+
+        filter_cells_options (FilterCellsOptions):
+            Options to pass to :py:meth:`~scranpy.quality_control.filter_cells.filter_cells`.
+
+        log_norm_counts_options (LogNormCountsOptions):
+            Options to pass to :py:meth:`~scranpy.normalization.log_norm_counts.log_norm_counts`.
+
+        choose_hvgs_options (ChooseHvgsOptions):
+            Options to pass to :py:meth:`~scranpy.feature_selection.choose_hvgs.choose_hvgs`.
+
+        model_gene_variances_options (ModelGeneVariancesOptions):
+            Options to pass to :py:meth:`~scranpy.feature_selection.model_gene_variances.model_gene_variances`.
+
+        run_pca_options (RunPcaOptions):
+            Options to pass to :py:meth:`~scranpy.dimensionality_reduction.run_pca.run_pca`.
+
+        build_neighbor_index_options (BuildNeighborIndexOptions):
+            Options to pass to :py:meth:`~scranpy.nearest_neighbors.build_neighbor_index.build_neighbor_index`.
+
+        find_nearest_neighbors_options (FindNearestNeighborsOptions):
+            Options to pass to :py:meth:`~scranpy.nearest_neighbors.find_nearest_neighbors.find_nearest_neighbors`.
+
+        run_tsne_options (RunTsneOptions):
+            Options to pass to :py:meth:`~scranpy.dimensionality_reduction.run_tsne.run_tsne`.
+
+        run_umap_options (RunUmapOptions):
+            Options to pass to :py:meth:`~scranpy.dimensionality_reduction.run_umap.run_umap`.
+
+        build_snn_graph_options (BuildSnnGraphOptions):
+            Options to pass to :py:meth:`~scranpy.clustering.build_snn_graph.build_snn_graph`.
+
+        score_markers_options (ScoreMarkersOptions):
+            Options to pass to :py:meth:`~scranpy.marker_detection.score_markers.score_markers`.
+
+        miscellaneous_options (MiscellaneousOptions):
+            Further options that are not associated with any single function call.
     """
 
     per_cell_rna_qc_metrics_options: qc.PerCellRnaQcMetricsOptions = field(
@@ -106,31 +150,23 @@ class AnalyzeOptions:
     # Multi-step setters.
     def set_seed(self, seed: int = 42):
         """Set seed for RNG.
+        This calls the method of the same name for
+        :py:meth:`~scranpy.dimensionality_reduction.run_pca.run_pca`,
+        :py:meth:`~scranpy.dimensionality_reduction.run_tsne.run_tsne`,
+        :py:meth:`~scranpy.dimensionality_reduction.run_umap.run_umap`.
 
         Args:
-            seed (int, optional): Seed for random number generation.
-                Defaults to 42.
+            seed (int, optional): 
+                Seed for random number generation.
         """
         self.run_pca_options.set_seed(seed)
         self.run_tsne_options.set_seed(seed)
         self.run_umap_options.set_seed(seed)
 
-    def set_block(self, block: Optional[Sequence] = None):
-        """Set block assignments for each cell.
-
-        Args:
-            block (Sequence, optional): Blocks assignments
-                for each cell. Defaults to None.
-        """
-        self.suggest_rna_qc_filters_options.set_block(block)
-        self.create_rna_qc_filter_options.set_block(block)
-        self.log_norm_counts_options.set_block(block)
-        self.model_gene_variances_options.set_block(block)
-        self.run_pca_options.set_block(block)
-        self.score_markers_options.set_block(block)
 
     def set_verbose(self, verbose: bool = False):
         """Set verbose to display logs.
+        This calls the method of the same name for all ``*_options`` objects.
 
         Args:
             verbose (bool, optional): Whether to print logs.
@@ -151,6 +187,21 @@ class AnalyzeOptions:
 
     def set_threads(self, num_threads: int = 1):
         """Set number of threads to use.
+        This calls the method of the same name for 
+        :py:meth:`~scranpy.quality_control.rna.per_cell_rna_qc_metrics`,
+        :py:meth:`~scranpy.normalization.log_norm_counts.log_norm_counts.`,
+        :py:meth:`~scranpy.feature_selection.model_gene_variances`,
+        :py:meth:`~scranpy.dimensionality_reduction.run_pca.run_pca`,
+        :py:meth:`~scranpy.nearest_neighbors.find_nearest_neighbors.find_nearest_neighbors`.
+
+        The number of threads provided to 
+        :py:meth:`~scranpy.nearest_neighbors.find_nearest_neighbors.find_nearest_neighbors`
+        is also used in :py:meth:`~scranpy.analyze.run_neighbor_suite`,
+        which determines the thread allocation to subsequent steps like
+        :py:meth:`~scranpy.dimensionality_reduction.run_tsne.run_tsne`
+        and 
+        :py:meth:`~scranpy.marker_detection.score_markers.score_markers`.
+        In all cases, thread utilization will not exceed the limit specified here in ``num_threads``.
 
         Args:
             num_threads (int, optional): Number of threads. Defaults to 1.
@@ -160,7 +211,4 @@ class AnalyzeOptions:
         self.choose_hvgs_options.set_threads(num_threads)
         self.model_gene_variances_options.set_threads(num_threads)
         self.run_pca_options.set_threads(num_threads)
-        self.run_tsne_options.set_threads(num_threads)
-        self.run_umap_options.set_threads(num_threads)
-        self.build_snn_graph_options.set_threads(num_threads)
-        self.score_markers_options.set_threads(num_threads)
+        self.find_nearest_neighbors.set_threads(num_threads)
