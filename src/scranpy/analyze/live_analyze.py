50c8116269ab5374a7c93283fedd13a2e946f42c
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-09-15 13:43:46 -0700

    Begin working on the overlord.

--- src/scranpy/analyze/live_analyze.py
+++ src/scranpy/analyze/live_analyze.py
@@ -21,8 +21,12 @@ __license__ = "MIT"
 
 
 def live_analyze(
-    matrix: MatrixTypes,
-    features: Sequence[str],
+    rna_matrix: Optional[MatrixTypes],
+    rna_features: Optional[Sequence[str]],
+    adt_matrix: Optional[MatrixTypes],
+    adt_features: Optional[Sequence[str]],
+    crispr_matrix: Optional[MatrixTypes],
+    crispr_matrix: Optional[Sequence[str]],
     options: AnalyzeOptions = AnalyzeOptions(),
 ) -> AnalyzeResults:
     ptr = tatamize(matrix)
@@ -31,37 +35,92 @@ def live_analyze(
             "Length of `features` not same as number of `rows` in the matrix."
         )
 
+    do_rna = rna_matrix not is None
+    do_adt = adt_matrix not is None
+    do_crispr = crispr_matrix not is None
+    do_multiple = (do_rna + do_adt + do_crispr) > 1
+
     # Start of the capture.
     results = AnalyzeResults()
 
-    subsets = {}
-    if isinstance(options.miscellaneous_options.mito_prefix, str):
-        subsets["mito"] = qc.guess_mito_from_symbols(
-            features, options.miscellaneous_options.mito_prefix
+    if do_rna:
+        subsets = {}
+        if isinstance(options.miscellaneous_options.mito_prefix, str):
+            subsets["mito"] = qc.guess_mito_from_symbols(
+                features, options.miscellaneous_options.mito_prefix
+            )
+        results.rna_quality_control_subsets = subsets
+
+        results.rna_quality_control_metrics = qc.per_cell_rna_qc_metrics(
+            matrix, options=update(options.per_cell_rna_qc_metrics_options, subsets=subsets)
         )
-    results.rna_quality_control_subsets = subsets
 
-    results.rna_quality_control_metrics = qc.per_cell_rna_qc_metrics(
-        matrix, options=update(options.per_cell_rna_qc_metrics_options, subsets=subsets)
-    )
+        results.rna_quality_control_thresholds = qc.suggest_rna_qc_filters(
+            results.rna_quality_control_metrics,
+            options=update(
+                options.suggest_rna_qc_filters_options,
+                block=options.miscellaneous_options.block,
+            ),
+        )
 
-    results.rna_quality_control_thresholds = qc.suggest_rna_qc_filters(
-        results.rna_quality_control_metrics,
-        options=update(
-            options.suggest_rna_qc_filters_options,
-            block=options.miscellaneous_options.block,
-        ),
-    )
+        results.rna_quality_control_filter = qc.create_rna_qc_filter(
+            results.rna_quality_control_metrics,
+            results.rna_quality_control_thresholds,
+            options=update(
+                options.create_rna_qc_filter_options,
+                block=options.miscellaneous_options.block,
+            ),
+        )
 
-    results.rna_quality_control_filter = qc.create_rna_qc_filter(
-        results.rna_quality_control_metrics,
-        results.rna_quality_control_thresholds,
-        options=update(
-            options.create_rna_qc_filter_options,
-            block=options.miscellaneous_options.block,
-        ),
-    )
+    if do_adt:
+        subsets = {}
+        # TODO: store the ADT stuff in here.
+        results.adt_quality_control_subsets = subsets
 
+        results.adt_quality_control_metrics = qc.per_cell_adt_qc_metrics(
+            matrix, options=update(options.per_cell_adt_qc_metrics_options, subsets=subsets)
+        )
+
+        results.adt_quality_control_thresholds = qc.suggest_adt_qc_filters(
+            results.adt_quality_control_metrics,
+            options=update(
+                options.suggest_adt_qc_filters_options,
+                block=options.miscellaneous_options.block,
+            ),
+        )
+
+        results.adt_quality_control_filter = qc.create_adt_qc_filter(
+            results.adt_quality_control_metrics,
+            results.adt_quality_control_thresholds,
+            options=update(
+                options.create_adt_qc_filter_options,
+                block=options.miscellaneous_options.block,
+            ),
+        )
+
+    if do_crispr:
+        results.crispr_quality_control_metrics = qc.per_cell_crispr_qc_metrics(
+            matrix, options=update(options.per_cell_crispr_qc_metrics_options, subsets=subsets)
+        )
+
+        results.crispr_quality_control_thresholds = qc.suggest_crispr_qc_filters(
+            results.crispr_quality_control_metrics,
+            options=update(
+                options.suggest_crispr_qc_filters_options,
+                block=options.miscellaneous_options.block,
+            ),
+        )
+
+        results.crispr_quality_control_filter = qc.create_crispr_qc_filter(
+            results.crispr_quality_control_metrics,
+            results.crispr_quality_control_thresholds,
+            options=update(
+                options.create_crispr_qc_filter_options,
+                block=options.miscellaneous_options.block,
+            ),
+        )
+
+    # TODO: add other filters here.
     filtered = qc.filter_cells(ptr, filter=results.rna_quality_control_filter)
 
     keep = numpy.logical_not(results.rna_quality_control_filter)
@@ -73,42 +132,112 @@ def live_analyze(
     else:
         filtered_block = None
 
-    if options.log_norm_counts_options.size_factors is None:
-        raw_size_factors = results.rna_quality_control_metrics.column("sums")[keep]
-    else:
-        raw_size_factors = options.log_norm_counts_options.size_factors
+    if do_rna:
+        if options.rna_log_norm_counts_options.size_factors is None:
+            raw_size_factors = results.rna_quality_control_metrics.column("sums")[keep]
+        else:
+            raw_size_factors = options.rna_log_norm_counts_options.size_factors
 
-    normed, final_size_factors = norm.log_norm_counts(
-        filtered,
-        options=update(
-            options.log_norm_counts_options,
-            size_factors=raw_size_factors,
-            center_size_factors_options=update(
-                options.log_norm_counts_options.center_size_factors_options,
-                block=filtered_block,
+        normed, final_size_factors = norm.log_norm_counts(
+            filtered,
+            options=update(
+                options.rna_log_norm_counts_options,
+                size_factors=raw_size_factors,
+                center_size_factors_options=update(
+                    options.rna_log_norm_counts_options.center_size_factors_options,
+                    block=filtered_block,
+                ),
+                with_size_factors=True,
             ),
-            with_size_factors=True,
-        ),
-    )
+        )
 
-    results.size_factors = final_size_factors
+        results.rna_size_factors = final_size_factors
 
-    results.gene_variances = feat.model_gene_variances(
-        normed,
-        options=update(options.model_gene_variances_options, block=filtered_block),
-    )
+        results.rna_gene_variances = feat.model_gene_variances(
+            normed,
+            options=update(options.model_gene_variances_options, block=filtered_block),
+        )
 
-    results.hvgs = feat.choose_hvgs(
-        results.gene_variances.column("residuals"),
-        options=options.choose_hvgs_options,
-    )
+        results.rna_hvgs = feat.choose_hvgs(
+            results.gene_variances.column("residuals"),
+            options=options.choose_hvgs_options,
+        )
 
-    results.pca = dimred.run_pca(
-        normed,
-        options=update(
-            options.run_pca_options, subset=results.hvgs, block=filtered_block
-        ),
-    )
+        results.rna_pca = dimred.run_pca(
+            normed,
+            options=update(
+                options.run_pca_options, subset=results.hvgs, block=filtered_block
+            ),
+        )
+
+    if do_adt:
+        if options.adt_log_norm_counts_options.size_factors is None:
+            raw_size_factors = results.adt_quality_control_metrics.column("sums")[keep]
+        else:
+            raw_size_factors = options.adt_log_norm_counts_options.size_factors
+
+        normed, final_size_factors = norm.log_norm_counts(
+            filtered,
+            options=update(
+                options.adt_log_norm_counts_options,
+                size_factors=raw_size_factors,
+                center_size_factors_options=update(
+                    options.adt_log_norm_counts_options.center_size_factors_options,
+                    block=filtered_block,
+                ),
+                with_size_factors=True,
+            ),
+        )
+
+        results.adt_size_factors = final_size_factors
+
+        # TODO: protect against not enough PCs.
+        results.adt_pca = dimred.run_pca(
+            normed,
+            options=update(
+                options.adt_run_pca_options, block=filtered_block
+            ),
+        )
+
+    if do_crispr:
+        if options.crispr_log_norm_counts_options.size_factors is None:
+            raw_size_factors = results.crispr_quality_control_metrics.column("sums")[keep]
+        else:
+            raw_size_factors = options.crispr_log_norm_counts_options.size_factors
+
+        normed, final_size_factors = norm.log_norm_counts(
+            filtered,
+            options=update(
+                options.crispr_log_norm_counts_options,
+                size_factors=raw_size_factors,
+                center_size_factors_options=update(
+                    options.crispr_log_norm_counts_options.center_size_factors_options,
+                    block=filtered_block,
+                ),
+                with_size_factors=True,
+            ),
+        )
+
+        results.crispr_pca = dimred.run_pca(
+            normed,
+            options=update(
+                options.run_pca_options, subset=results.hvgs, block=filtered_block
+            ),
+        )
+
+    if do_multiple:
+        all_embeddings = []
+        if do_rna:
+            all_embeddings.append(results.rna_pca.principal_components)
+        if do_adt:
+            all_embeddings.append(results.adt_pca.principal_components)
+        if do_crispr:
+            all_embeddings.append(results.crispr_pca.principal_components)
+
+        results.combined_embeddings = dimred.combine_embeddings(
+            all_embeddings,
+            options=options.combine_embeddings_options
+        )
 
     if options.miscellaneous_options.block is not None:
         results.mnn = correct.mnn_correct(
@@ -135,15 +264,38 @@ def live_analyze(
         resolution=options.miscellaneous_options.snn_graph_multilevel_resolution
     ).membership
 
-    results.markers = mark.score_markers(
-        normed,
-        grouping=results.clusters,
-        options=update(
-            options.score_markers_options,
-            block=filtered_block,
-            num_threads=remaining_threads,
-        ),
-    )
+    if do_rna:
+        results.rna_markers = mark.score_markers(
+            normed,
+            grouping=results.clusters,
+            options=update(
+                options.rna_score_markers_options,
+                block=filtered_block,
+                num_threads=remaining_threads,
+            ),
+        )
+
+    if do_adt:
+        results.adt_markers = mark.score_markers(
+            normed,
+            grouping=results.clusters,
+            options=update(
+                options.adt_score_markers_options,
+                block=filtered_block,
+                num_threads=remaining_threads,
+            ),
+        )
+
+    if do_crispr:
+        results.crispr_markers = mark.score_markers(
+            normed,
+            grouping=results.clusters,
+            options=update(
+                options.crispr_score_markers_options,
+                block=filtered_block,
+                num_threads=remaining_threads,
+            ),
+        )
 
     results.tsne = get_tsne()
     results.umap = get_umap()
