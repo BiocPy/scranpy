090219de21ceeb9236de8803710701bdf2b15814
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-09-18 14:15:39 -0700

    Got everything to compile, more or less.

--- src/scranpy/normalization/grouped_size_factors.py
+++ src/scranpy/normalization/grouped_size_factors.py
@@ -8,8 +8,8 @@ class GroupedSizeFactorsOptions:
     """Options to pass to :py:meth:`~scranpy.grouped_size_factors.grouped_size_factors`.
 
     Attributes:
-        clusters (Sequence, optional):
-            Sequence of cluster assignments of length equal to the number of cells.
+        groups (Sequence, optional):
+            Sequence of group assignments, of length equal to the number of cells.
 
         rank (int): 
             Number of principal components to obtain in the low-dimensional
@@ -23,13 +23,34 @@ class GroupedSizeFactorsOptions:
             Number of threads to use for the various calculations.
     """
     rank: int = 25
-    clusters: Optional[Sequence] = None
+    groups: Optional[Sequence] = None
     block: Optional[Sequence] = None
     assay_type: Union[int, str] = 0
     num_threads: int = 1
 
 
-def grouped_size_factors(input: MatrixTypes, options: GroupedSizeFactorsOptions = GroupedSizeFactorsOptions()):
+def grouped_size_factors(input: MatrixTypes, options: GroupedSizeFactorsOptions = GroupedSizeFactorsOptions()) -> ndarray:
+    """Compute grouped size factors to remove composition biases between groups of cells.
+    This sums all cells from the same group into a pseudo-cell, applies median-based normalization between pseudo-cells,
+    and propagates the pseudo-cell size factors back to each cell via library size scaling.
+
+    Args:
+        input (MatrixTypes): 
+            Matrix-like object where rows are features and columns are cells, typically containing
+            expression values of some kind. This should be a matrix class that can be converted into a
+            :py:class:`~mattress.TatamiNumericPointer.TatamiNumericPointer`.
+
+            Alternatively, a :py:class:`~summarizedexperiment.SummarizedExperiment.SummarizedExperiment`
+            containing such a matrix in its assays.
+
+            Developers may also provide a :py:class:`~mattress.TatamiNumericPointer.TatamiNumericPointer` directly.
+
+        options (GroupedSizeFactorsOptions):
+            Further options.
+
+    Returns:
+        ndarray: Array of size factors for each cell in ``input``.
+    """
     ptr = tatamize_input(input, options.assay_type)
     output = ndarray(ptr.ncol(), dtype=float64)
 
@@ -40,6 +61,6 @@ def grouped_size_factors(input: MatrixTypes, options: GroupedSizeFactorsOptions 
         lib.grouped_size_factors_with_clusters(ptr.ptr, output, options.num_threads)
     else:
         use_block, num_blocks, block_names, block_info, block_offset = process_block(options.block, NC)
-        lib.grouped_size_factors_without_cluster(ptr.ptr, use_block, block_offset, options.rank, output, options.num_threads)
+        lib.grouped_size_factors_without_clusters(ptr.ptr, use_block, block_offset, options.rank, output, options.num_threads)
 
     return output
